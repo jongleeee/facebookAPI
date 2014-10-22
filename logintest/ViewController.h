a66ed5cfe3588c6db3ba02e0e32b835bc27f0a93
jongleee <lee.jongyun13@gmail.com>
2014-10-22 12:32:00 -0700

    setting up

---
+++ logintest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  logintest
+//
+//  Created by Jong Yun Lee on 10/22/14.
+//  Copyright (c) 2014 HeapStack. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ logintestTests/logintestTests.m
@@ -0,0 +1,40 @@
+//
+//  logintestTests.m
+//  logintestTests
+//
+//  Created by Jong Yun Lee on 10/22/14.
+//  Copyright (c) 2014 HeapStack. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface logintestTests : XCTestCase
+
+@end
+
+@implementation logintestTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
